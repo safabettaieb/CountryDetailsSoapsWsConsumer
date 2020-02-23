482540c04149d06827c5cb405398266f1a18e295
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-23 22:11:54 +0100

    create CountryService class and getCountryDetails method

---
+++ src/main/java/com/safa/country/soap/ws/services/CountryService.java
@@ -0,0 +1,26 @@
+package com.safa.country.soap.ws.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Service;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+import com.safa.country.soap.ws.countrydetails.CountryDetailsRequest;
+import com.safa.country.soap.ws.countrydetails.GetCountryResponse;
+
+@Service
+public class CountryService {
+	@Autowired
+	private Jaxb2Marshaller marshaller;
+	private WebServiceTemplate template;
+
+	private final String URL = "http://localhost:8081/ws";
+
+	public GetCountryResponse getCountryDetails(CountryDetailsRequest request) {
+
+		template = new WebServiceTemplate(marshaller);
+		GetCountryResponse response = (GetCountryResponse) template.marshalSendAndReceive(URL, request);
+		return response;
+	}
+
+}

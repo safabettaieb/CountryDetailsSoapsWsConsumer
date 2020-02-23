1a6e3bc60d6b176ab5b76d5d7d0f26f234fb336b
safa bettieb <safa.bettaieb1@gmail.com>
2020-02-23 21:18:36 +0100

    first commit

---
+++ src/main/java/com/safa/country/soap/ws/CountryDetailsSoapsWsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.safa.country.soap.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryDetailsSoapsWsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryDetailsSoapsWsConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safa/country/soap/ws/CountryDetailsSoapsWsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.country.soap.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryDetailsSoapsWsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
